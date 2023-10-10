feb3e759f1c29e41bb06c13cb7c72f834849c932
st3b1t <st3b1t@pm.me>
2023-10-10 22:41:06 +0200

    parse-items

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 
 const fs = require('fs')
     , S = require('fluent-json-schema')
-    , {default: {parse}} = require('restructured');
+    , {default: rstParser} = require('restructured');
 
-const rstUrl = 'https://raw.githubusercontent.com/bitcoin-dot-org/developer.bitcoin.org/master/reference/rpc/index.rst'
+const baseUrl = 'https://raw.githubusercontent.com/bitcoin-dot-org/developer.bitcoin.org/master/reference/rpc';
+//TODO get tag version
 
-fetch(rstUrl)
+const openapi = {
+    "openapi": "3.0.3",
+    "info": {
+        "title": "Bitcoin RPC API Reference",
+        "description": "",
+        "version": ""
+    },
+    "externalDocs": {
+        "description": "This site aims to provide the docs you need to understand Bitcoin and start building Bitcoin-based applications.",
+        "url": "https://developer.bitcoin.org/reference/rpc/"
+    },
+    "components": {
+        "schemas": {}
+    },
+    "paths": {}
+}
+
+/*const schema = S.object()
+.prop('paths', S.array())
+console.log(schema.valueOf())*/
+
+fetch(`${baseUrl}/index.rst`)
 .then(res => res.text())
 .then(body => {
 
-    const parsed = parse(body);
+    const parsed = rstParser.parse(body);
 
-    console.log(JSON.stringify(parsed,null,4));
+    //fs.writeFileSync('dist/index.rst.json', JSON.stringify(parsed,null,4))
+
+    const sections = [];
+    parsed.children[1].children.forEach(item => {
+        if (item.type === 'section') {
+            //console.log(item)
+            item.children.forEach(item2 => {
+                if (item2.type === 'directive') {
+                    item2.children.forEach(item3 => {
+                        if (item3.value && !item3.value.includes(':maxdepth:')) {
+                            console.log(item3)
+                        }
+                    })
+                }
+            })
+        }
+    })
 });
+
