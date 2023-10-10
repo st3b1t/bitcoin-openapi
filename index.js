32ced58aefd078d98cdcef539f46d139645ed334
st3b1t <st3b1t@pm.me>
2023-10-10 12:34:46 +0200

    json

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-const {default: {parse}} = require('restructured');
-const S = require('fluent-json-schema');
+const fs = require('fs')
+    , S = require('fluent-json-schema')
+    , {default: {parse}} = require('restructured');
 
 const rstUrl = 'https://raw.githubusercontent.com/bitcoin-dot-org/developer.bitcoin.org/master/reference/rpc/index.rst'
 
 fetch(rstUrl)
 .then(res => res.text())
 .then(body => {
-    console.log(body);
+
     const parsed = parse(body);
 
     console.log(JSON.stringify(parsed,null,4));
